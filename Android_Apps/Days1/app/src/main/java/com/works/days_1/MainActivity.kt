64f8e260d682a7889c4e9308e310bed605b86db7
Shyonei <akinemreyazici01@gmail.com>
2023-04-07 22:59:33 +0300

    Days-2

--- Android_Apps/Days1/app/src/main/java/com/works/days_1/MainActivity.kt
+++ Android_Apps/Days1/app/src/main/java/com/works/days_1/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.works.days_1
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextWatcher
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var btnGotoSettings : Button
     lateinit var txtData : EditText
+    lateinit var txtViewData : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +24,41 @@ class MainActivity : AppCompatActivity() {
 
         txtData = findViewById(R.id.txtData)
         btnGotoSettings = findViewById(R.id.btnGotoSettings)
+        txtViewData = findViewById(R.id.txtViewData)
+
+        var btnColor = btnGotoSettings.backgroundTintList?.defaultColor
 
         btnGotoSettings.setOnClickListener {
             var data = txtData.text.toString()
             Toast.makeText(this,data,Toast.LENGTH_LONG).show()
         }
-    }
 
+        // OnChange
+        txtData.addTextChangedListener {
+
+            txtViewData.text = it.toString()
+            if(it.toString() == "zehra@mail.com")
+            {
+                btnGotoSettings.setBackgroundColor(Color.RED)
+            }
+            else
+            {
+                btnGotoSettings.setBackgroundColor(btnColor!!)
+            }
+        }
+
+
+
+    }
+    /*
+    fun bttnClick(view : View) : Unit {
+        view.setBackgroundColor(Color.RED)
+        var btn: Button = view as Button
+        var title = btn.text
+        Log.i("",title.toString())
+        var data = txtData.text.toString()
+        Toast.makeText(this,"bttnClick : " +data,Toast.LENGTH_LONG).show()
+    }
+     */
 }
+
---
+++ Android_Apps/Days2/app/src/main/java/com/works/days_2/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.works.days_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var btnToast : Button
+    lateinit var customToastView : View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnToast = findViewById(R.id.btnToast)
+
+        btnToast.setOnClickListener(btnToastClickListener)
+
+        customToastView = layoutInflater.inflate(R.layout.custom_toast,null)
+        customToastView.findViewById<>(R.id.customToastID)
+       // var toast = Toast.makeText(this,"Toast Title",Toast.LENGTH_SHORT)
+       // toast.setGravity(Gravity.LEFT,200,200)
+       // toast.show()
+    }
+
+    val btnToastClickListener = View.OnClickListener {
+       // Toast.makeText(this,"Bttn Toast",Toast.LENGTH_LONG).show()
+        var toast = Toast(applicationContext)
+        toast.duration = Toast.LENGTH_LONG
+        toast.view = customToastView
+        toast.show()
+
+    }
+
+
+}
