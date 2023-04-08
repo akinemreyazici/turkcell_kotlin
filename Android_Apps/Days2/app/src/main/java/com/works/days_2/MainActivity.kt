874f523c053c715c13d0a5c2a692d92738d46e2a
Shyonei <akinemreyazici01@gmail.com>
2023-04-08 19:58:41 +0300

    After Ödev1

--- Android_Apps/Days2/app/src/main/java/com/works/days_2/MainActivity.kt
+++ Android_Apps/Days2/app/src/main/java/com/works/days_2/MainActivity.kt
@@ -6,6 +6,8 @@ import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -14,16 +16,20 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var btnToast : Button
     lateinit var customToastView : View
+    lateinit var txtToastInformation : EditText
+    lateinit var alertText : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnToast = findViewById(R.id.btnToast)
+        txtToastInformation = findViewById(R.id.txtToastInformation)
 
         btnToast.setOnClickListener(btnToastClickListener)
 
         customToastView = layoutInflater.inflate(R.layout.custom_toast,null)
-        customToastView.findViewById<>(R.id.customToastID)
+        customToastView.findViewById<ViewGroup>(R.id.customToastID)
+        alertText = customToastView.findViewById(R.id.txtToast)
        // var toast = Toast.makeText(this,"Toast Title",Toast.LENGTH_SHORT)
        // toast.setGravity(Gravity.LEFT,200,200)
        // toast.show()
@@ -31,10 +37,20 @@ class MainActivity : AppCompatActivity() {
 
     val btnToastClickListener = View.OnClickListener {
        // Toast.makeText(this,"Bttn Toast",Toast.LENGTH_LONG).show()
-        var toast = Toast(applicationContext)
+       /* var toast = Toast(applicationContext)
         toast.duration = Toast.LENGTH_LONG
         toast.view = customToastView
         toast.show()
+        */
+
+        // Ödev1 in gereksinimleri aşağıdaki kod satırlarında yapılmıştır.
+        var text = txtToastInformation.text.toString()
+        var toast = Toast(applicationContext)
+        alertText.text = txtToastInformation.text
+        toast.duration = Toast.LENGTH_LONG
+        toast.view = customToastView
+        toast.show()
+
 
     }
 
