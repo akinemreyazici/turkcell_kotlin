3361679e226c06658d91a68af41ade21704580e9
Shyonei <akinemreyazici01@gmail.com>
2023-04-15 23:14:27 +0300

    Days-6

---
+++ Android_Apps/Days6/app/src/main/java/com/works/days_6/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.works.days_6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ListView
+import com.works.days_6.adapter.UserCustomAdapter
+import com.works.days_6.services.UserService
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var listView : ListView
+    val userService = UserService()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        listView = findViewById(R.id.userListView)
+        val customAdapter = UserCustomAdapter(this,userService.userResult())
+        listView.adapter = customAdapter
+
+    }
+}
---
+++ Android_Apps/Days6/app/src/main/java/com/works/days_6/models/User.kt
@@ -0,0 +1,9 @@
+package com.works.days_6.models
+
+data class User(
+    var first: String,
+    var last:String,
+    var email:String,
+    var age:Int,
+    var image:String
+)
